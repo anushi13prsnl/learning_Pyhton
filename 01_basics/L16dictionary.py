602b0286088097d08c2df8d16d011b27387988c3
Anushi Rajput <145750945+anushi13prsnl@users.noreply.github.com>
2023-11-26 07:20:58 +0000

    methodsOfDict

--- 01_basics/L16dictionary.py
+++ 01_basics/L16dictionary.py
@@ -54,3 +54,71 @@ dict_items([('name', 'Karan'), ('age', 19), ('eligible', True)])
 
 # for key, value in info.items():
 #   print(f"The value corresponding to the key {key} is {value}") 
+'''
+* Dictionary Methods:Dictionary uses several built-in methods for manipulation.They are listed below
+
+1)update()
+The update() method updates the value of the key provided to it if the item already exists in the dictionary, else it creates a new key-value pair.
+
+Example:
+info = {'name':'Karan', 'age':19, 'eligible':True}
+print(info)
+info.update({'age':20})
+info.update({'DOB':2001})
+print(info)
+Output:
+{'name': 'Karan', 'age': 19, 'eligible': True}
+{'name': 'Karan', 'age': 20, 'eligible': True, 'DOB': 2001}
+
+Removing items from dictionary:
+There are a few methods that we can use to remove items from dictionary.
+
+2)clear():
+The clear() method removes all the items from the list.
+
+Example:
+info = {'name':'Karan', 'age':19, 'eligible':True}
+info.clear()
+print(info)
+Output:
+{} #empty dictionary
+
+3)pop():
+The pop() method removes the key-value pair whose key is passed as a parameter.
+
+Example:
+info = {'name':'Karan', 'age':19, 'eligible':True}
+info.pop('eligible')
+print(info)
+Output:
+{'name': 'Karan', 'age': 19}
+
+4)popitem():
+The popitem() method removes the last key-value pair from the dictionary.
+
+Example:
+info = {'name':'Karan', 'age':19, 'eligible':True, 'DOB':2003}
+info.popitem()
+print(info)
+Output:
+{'name': 'Karan', 'age': 19, 'eligible': True}
+
+5)del:
+we can also use the del keyword to remove a dictionary item.
+
+Example:
+info = {'name':'Karan', 'age':19, 'eligible':True, 'DOB':2003}
+del info['age']
+print(info)
+Output:
+{'name': 'Karan', 'eligible': True, 'DOB': 2003}
+
+If key is not provided, then the del keyword will delete the dictionary entirely.
+
+Example:
+info = {'name':'Karan', 'age':19, 'eligible':True, 'DOB':2003}
+del info
+print(info)
+Output:
+NameError: name 'info' is not defined
+'''
